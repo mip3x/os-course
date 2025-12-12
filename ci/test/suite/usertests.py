8761267003c80f737e504a708708ce5adfcd6133
mip3x <plombiiiri@yandex.ru>
2025-12-12 03:32:39 +0300

    feat: spinlock on proc in reparent()

--- ci/test/suite/usertests.py
+++ ci/test/suite/usertests.py
@@ -38,7 +38,7 @@ QUICK_TESTS = [
     Xv6UserTest(name="twochildren", timeout=timedelta(seconds=10)),
     Xv6UserTest(name="forkfork", timeout=timedelta(seconds=3)),
     Xv6UserTest(name="forkforkfork", timeout=timedelta(seconds=8)),
-    Xv6UserTest(name="reparent2", timeout=timedelta(seconds=150)),
+    Xv6UserTest(name="reparent2", timeout=timedelta(seconds=15)),
     Xv6UserTest(name="mem", timeout=timedelta(seconds=5)),
     Xv6UserTest(name="sharedfd", timeout=timedelta(seconds=36)),
     Xv6UserTest(name="fourfiles", timeout=timedelta(seconds=5)),
--- kernel/proc/proc.c
+++ kernel/proc/proc.c
@@ -324,15 +324,25 @@ void reparent(struct proc *p) {
     struct list *iter;
     acquire(&proc_lst_lock);
 
-    for (iter = proc_lst_head.next; iter != &proc_lst_head; iter = iter->next) {
+    for (iter = proc_lst_head.next; iter != &proc_lst_head;) {
         struct proc *pp = (struct proc *)iter;
+
+        acquire(&pp->lock);
+        iter = iter->next;
+
         release(&proc_lst_lock);
 
         if (pp->parent == p) {
             pp->parent = initproc;
+
+            release(&pp->lock);
             wakeup(initproc);
+            acquire(&proc_lst_lock);
+
+            continue;
         }
 
+        release(&pp->lock);
         acquire(&proc_lst_lock);
     }
 
