cf04039a120c8ef94919b0bc823214ee565d39f6
mip3x <plombiiiri@yandex.ru>
2026-01-09 19:05:40 +0300

    feat: print_bits implemented

--- kernel/defs.h
+++ kernel/defs.h
@@ -87,6 +87,7 @@ int pipewrite(struct pipe *, uint64, int);
 int printf(char *, ...) __attribute__((format(printf, 1, 2)));
 void panic(char *) __attribute__((noreturn));
 void printfinit(void);
+void print_bits(uint x, int n);
 
 // proc.c
 int cpuid(void);
