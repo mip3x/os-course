3b0e6e6d9f17cac9e8f018106fa8abe8330415f6
mip3x <plombiiiri@yandex.ru>
2026-01-09 20:24:28 +0300

    feat: cancel PTE_W for parent and child pages

--- kernel/memory/vm.c
+++ kernel/memory/vm.c
@@ -310,27 +310,28 @@ int uvmcopy(pagetable_t old, pagetable_t new, uint64 sz) {
     pte_t *pte;
     uint64 pa, i;
     uint flags;
-    char *mem;
 
     for (i = 0; i < sz; i += PGSIZE) {
         if ((pte = walk(old, i, 0)) == 0)
             panic("uvmcopy: pte should exist");
         if ((*pte & PTE_V) == 0)
             panic("uvmcopy: page not present");
+        // clear PTE_W for both parent and child
+        *pte &= ~PTE_W;
+
         pa = PTE2PA(*pte);
         flags = PTE_FLAGS(*pte);
-        if ((mem = kalloc(PGSIZE)) == 0)
-            goto err;
-        memmove(mem, (char *)pa, PGSIZE);
-        if (mappages(new, i, PGSIZE, (uint64)mem, flags) != 0) {
-            kfree(mem);
+
+        printf("uvmcopy: pa 0x%lx flags 0b", pa);
+        print_bits(flags, 10);
+
+        if (mappages(new, i, PGSIZE, (uint64)pa, flags) != 0) {
             goto err;
         }
     }
     return 0;
 
 err:
-    uvmunmap(new, 0, i / PGSIZE, 1);
     return -1;
 }
 
