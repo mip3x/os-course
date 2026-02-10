bd5f5faa806c94843dace7839ebac248caa3c48c
mip3x <plombiiiri@yandex.ru>
2026-02-10 22:49:24 +0300

    chore: aslrcheck prints rodata

--- user/aslrcheck.c
+++ user/aslrcheck.c
@@ -6,11 +6,13 @@ int global;
 int main(void) {
     int local = 0;
     void *heap = malloc(16);
+    char *hello_string = "hello";
 
     printf("code main:   %p\n", main);
     printf("data global: %p\n", &global);
     printf("stack local: %p\n", &local);
     printf("heap malloc: %p\n", heap);
+    printf("ro data:     %p\n", hello_string);
 
     free(heap);
     exit(0);
